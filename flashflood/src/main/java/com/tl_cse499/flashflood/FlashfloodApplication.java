0a96f588b329b72d6279e3922184a5f53ff6fdce
Lee17026atBYUI <leetim@byui.edu>
2025-10-18 18:07:52 -0600

    w05 spring boot backend creation

---
+++ flashflood/src/main/java/com/tl_cse499/flashflood/FlashfloodApplication.java
@@ -0,0 +1,13 @@
+package com.tl_cse499.flashflood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashfloodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashfloodApplication.class, args);
+	}
+
+}
---
+++ flashflood/src/test/java/com/tl_cse499/flashflood/FlashfloodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tl_cse499.flashflood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashfloodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
